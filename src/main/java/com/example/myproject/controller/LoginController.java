25aea366e37923803f62bb808e5887fd2e33f863
고민봉 <sjy2017z@naver.com>
2024-04-02 23:20:26 +0900

    페이징 처리 구현

--- src/main/java/com/example/myproject/controller/LoginController.java
+++ src/main/java/com/example/myproject/controller/LoginController.java
@@ -3,7 +3,6 @@ package com.example.myproject.controller;
 import com.example.myproject.domain.Member;
 import com.example.myproject.domain.MusicList;
 import com.example.myproject.dto.LoginFormDto;
-import com.example.myproject.dto.PageDto;
 import com.example.myproject.service.MusicListService;
 import com.example.myproject.service.LoginService;
 import jakarta.servlet.http.HttpServletRequest;
@@ -12,13 +11,11 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import java.util.List;
+
 
 @Controller
 @RequiredArgsConstructor
@@ -28,12 +25,13 @@ public class LoginController {
     private final LoginService loginService;
     private final MusicListService musicListService;
 
+
     @GetMapping("/myInfo")
     public String myInfo(HttpServletRequest request, Model model) {
 
 
         HttpSession session = request.getSession(false);
-        if (session != null){
+        if (session != null) {
             model.addAttribute("loginId", session.getAttribute("loginId"));
         }
 
@@ -41,44 +39,42 @@ public class LoginController {
     }
 
     @GetMapping("/login")
-    public String loginForm( Model model) {
+    public String loginForm(Model model) {
 
-        model.addAttribute("loginFormDto",new LoginFormDto());
+        model.addAttribute("loginFormDto", new LoginFormDto());
         return "login/loginForm";
     }
 
     @GetMapping("/")
     public String loginSession(@RequestParam(value = "page", defaultValue = "0") int page,
-                               @RequestParam(value = "start", defaultValue = "0") int start,
-                               @RequestParam(value = "end", defaultValue = "6") int end,
-                               HttpServletRequest request, Model model){
-        model.addAttribute("menu","list");
+                               HttpServletRequest request, Model model) {
+        model.addAttribute("menu", "list");
         HttpSession session = request.getSession(false);
+        log.info("페이지 정보 확인 = {} ", page);
 
         if (session == null) {
             Page<MusicList> paging = musicListService.findAllItemList(page);
             model.addAttribute("paging", paging);
             model.addAttribute("page", page);
-            model.addAttribute("start", start);
-            model.addAttribute("end", end);
+            int temp=page/7;
+            int start = temp* 7;
+            log.info("스타트 위치 확인 해보기 = {} ", start);
 
-            log.info("스타트 시작번호 확인 하기 = {} ", start);
-            log.info("마지막 번호 확인하기 = {} ", end);
+            model.addAttribute("start", start);
+            model.addAttribute("end", start+6);
             return "home";
         }
         model.addAttribute("loginId", session.getAttribute("loginId"));
         Object id = session.getAttribute("id");
-
         Page<MusicList> paging = musicListService.findAllItemList(page);
-
         model.addAttribute("page", page);
         model.addAttribute("paging", paging);
+        int temp=page/7;
+        int start = temp* 7;
+        log.info("스타트 확인 해보기 = {} ", start);
+
         model.addAttribute("start", start);
-        model.addAttribute("end", end);
-
-        log.info("스타트 시작번호 확인 하기 = {} ", start);
-        log.info("마지막 번호 확인하기 = {} ", end);
-
+        model.addAttribute("end", start+6);
         return "home";
     }
 
